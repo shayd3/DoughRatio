9ab6ba9badfb74e38780fdc97399e4e44a6a592c
shayd3 <ryan.ross215@gmail.com>
2024-02-17 10:48:23 -0500

    add gramSuffix and percentSuffix

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,6 +3,9 @@ import InputNumber from 'primevue/inputnumber';
 
 import { ref } from "vue";
 
+const percentSuffix = " %";
+const gramsSuffix = " g";
+
 const flourWeight = ref(1000)
 const waterPercentage = ref(75)
 const saltPercentage = ref(2)
@@ -13,31 +16,32 @@ const leavenPercentage = ref(20)
   <h1>Bakers Calculator</h1>
   <div class="flex flex-column gap-3">
     <div>
-      <label for="horizontal-buttons" class="font-bold block mb-2"> Flour </label>
-      <InputNumber v-model="flourWeight" inputId="horizontal-buttons" showButtons buttonLayout="horizontal" :step="250"
-        :use-grouping="false">
-        <template #incrementbuttonicon>
-          <span class="pi pi-plus" />
-        </template>
-        <template #decrementbuttonicon>
-          <span class="pi pi-minus" />
-        </template>
-      </InputNumber>
+        <label for="flourWeight" class="font-bold block mb-2"> Flour </label>
+        <InputNumber v-model="flourWeight" inputId="flourWeight" :suffix="gramsSuffix" showButtons buttonLayout="horizontal" :step="250"
+          :use-grouping="false">
+          <template #incrementbuttonicon>
+            <span class="pi pi-plus" />
+          </template>
+          <template #decrementbuttonicon>
+            <span class="pi pi-minus" />
+          </template>
+        </InputNumber>
     </div>
 
     <div>
       <label for="waterPercentage" class="font-bold block mb-2"> Water </label>
-      <InputNumber v-model="waterPercentage" inputId="waterPercentage" suffix="%" :min="0" :use-grouping="false">
+      <InputNumber v-model="waterPercentage" inputId="waterPercentage" :suffix="percentSuffix" :min="0" :use-grouping="false">
       </InputNumber>
+
     </div>
     <div>
       <label for="saltPercentage" class="font-bold block mb-2"> Salt </label>
-      <InputNumber v-model="saltPercentage" inputId="saltPercentage" suffix="%" :min="0" :use-grouping="false">
+      <InputNumber v-model="saltPercentage" inputId="saltPercentage" :suffix="percentSuffix" :min="0" :use-grouping="false">
       </InputNumber>
     </div>
     <div>
       <label for="leavenPercentage" class="font-bold block mb-2"> Leaven </label>
-      <InputNumber v-model="leavenPercentage" inputId="leavenPercentage" suffix="%" :min="0" :use-grouping="false">
+      <InputNumber v-model="leavenPercentage" inputId="leavenPercentage" :suffix="percentSuffix" :min="0" :use-grouping="false">
       </InputNumber>
     </div>
   </div>
