4b73cc21fffb498d3a416b2647a72960438b2758
shayd3 <ryan.ross215@gmail.com>
2024-02-24 11:00:41 -0500

    formatting

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,7 +5,6 @@ import Button from 'primevue/button';
 import Checkbox from 'primevue/checkbox';
 import Dialog from 'primevue/dialog';
 
-
 import { ref, watch } from "vue";
 
 const percentSuffix = " %";
@@ -19,23 +18,23 @@ const flour = {
 }
 
 const ingredients = [
-  { name: "Water", weight: ref(750), percentage: ref(75), checked: ref(true)},
-  { name: "Salt", weight: ref(20), percentage: ref(2), checked: ref(true)},
-  { name: "Leaven", weight: ref(200), percentage: ref(20), checked: ref(true)},
-  { name: "Yeast", weight: ref(10), percentage: ref(1), checked: ref(true)},
-  { name: "Butter", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Starter", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Poolish", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Malt", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Sugar", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Milk", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Eggs", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Honey", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Vanilla", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Cinnamon", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Baking Powder", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Baking Soda", weight: ref(0), percentage: ref(0), checked: ref(false)},
-  { name: "Oil", weight: ref(0), percentage: ref(0), checked: ref(false)}
+  { name: "Water", weight: ref(750), percentage: ref(75), checked: ref(true) },
+  { name: "Salt", weight: ref(20), percentage: ref(2), checked: ref(true) },
+  { name: "Leaven", weight: ref(200), percentage: ref(20), checked: ref(true) },
+  { name: "Yeast", weight: ref(10), percentage: ref(1), checked: ref(true) },
+  { name: "Butter", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Starter", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Poolish", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Malt", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Sugar", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Milk", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Eggs", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Honey", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Vanilla", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Cinnamon", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Baking Powder", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Baking Soda", weight: ref(0), percentage: ref(0), checked: ref(false) },
+  { name: "Oil", weight: ref(0), percentage: ref(0), checked: ref(false) }
 ].sort((a, b) => a.name.localeCompare(b.name))
 
 const percentWatchList = ingredients.map(ingredient => ingredient.percentage);
@@ -43,16 +42,12 @@ percentWatchList.push(flour.weight)
 const weightWatchList = ingredients.map(ingredient => ingredient.weight)
 
 watch(percentWatchList, () => {
-  console.log("Percentage changed or flour weight changed")
-  // Calculate the weight of the ingredient
   ingredients.forEach(ingredient => {
     ingredient.weight.value = calculateWeight(ingredient.percentage.value)
   })
 })
 
 watch(weightWatchList, () => {
-  console.log("Weight changed")
-  // Calculate the percentage of the ingredient
   ingredients.forEach(ingredient => {
     ingredient.percentage.value = calculatePercentage(ingredient.weight.value)
   })
@@ -66,16 +61,15 @@ const calculatePercentage = (weight: number) => {
   return (weight / flour.weight.value) * 100;
 }
 
-
 </script>
 
 <template>
   <Panel>
-  <div class="flex flex-column gap-3">
-    <label for="flour" class="font-bold block"> Flour </label>
-    <div id="flour">
-        <InputNumber class="w-full" v-model="flour.weight.value" inputId="flourWeight" :suffix="gramsSuffix" showButtons buttonLayout="horizontal" :step="250"
-          :use-grouping="false">
+    <div class="flex flex-column gap-3">
+      <label for="flour" class="font-bold block"> Flour </label>
+      <div id="flour">
+        <InputNumber class="w-full" v-model="flour.weight.value" inputId="flourWeight" :suffix="gramsSuffix" showButtons
+          buttonLayout="horizontal" :step="250" :use-grouping="false">
           <template #incrementbuttonicon>
             <span class="pi pi-plus" />
           </template>
@@ -83,38 +77,41 @@ const calculatePercentage = (weight: number) => {
             <span class="pi pi-minus" />
           </template>
         </InputNumber>
-    </div>
+      </div>
 
-    <template v-for="ingredient in ingredients" :key="ingredient.name">
-      <template v-if="ingredient.checked.value">
-        <label :for="ingredient.name" class="font-bold block"> {{ ingredient.name }} </label>
-        <div :id="ingredient.name" class="flex flex-row gap-3">
-          <div>
-            <InputNumber v-model="ingredient.percentage.value" :suffix="percentSuffix" :min="0" :use-grouping="false"></InputNumber>
-          </div>
-          <div>
-            <InputNumber v-model="ingredient.weight.value" :suffix="gramsSuffix" :min="0" :use-grouping="false"></InputNumber>
-          </div>
-        </div>
-      </template>
-    </template>
-
-    <Button @click="ingredientDialogVisible = true" label="Add/Remove Ingredients" />
-    <Dialog v-model:visible="ingredientDialogVisible" modal header="Add/Remove Ingredients" :style="{ width: '25rem' }">
-      <div class="flex flex-column gap-3">
-        <template v-for="ingredient in ingredients" :key="ingredient.name">
-          <div class="flex flex-row gap-2">
-            <Checkbox v-model="ingredient.checked.value" :inputId="ingredient.name" :binary="true" :label="ingredient.name" />
-            <label :for="ingredient.name"> {{ ingredient.name }} </label>
+      <template v-for="ingredient in ingredients" :key="ingredient.name">
+        <template v-if="ingredient.checked.value">
+          <label :for="ingredient.name" class="font-bold block"> {{ ingredient.name }} </label>
+          <div :id="ingredient.name" class="flex flex-row gap-3">
+            <div>
+              <InputNumber v-model="ingredient.percentage.value" :suffix="percentSuffix" :min="0" :use-grouping="false">
+              </InputNumber>
+            </div>
+            <div>
+              <InputNumber v-model="ingredient.weight.value" :suffix="gramsSuffix" :min="0" :use-grouping="false">
+              </InputNumber>
+            </div>
           </div>
         </template>
-      </div>
-      <div class="flex justify-content-end gap-2">
-              <Button type="button" label="Save" @click="ingredientDialogVisible = false"></Button>
-          </div>
-    </Dialog>
+      </template>
 
-  </div>
+      <Button @click="ingredientDialogVisible = true" label="Add/Remove Ingredients" />
+      <Dialog v-model:visible="ingredientDialogVisible" modal header="Add/Remove Ingredients" :style="{ width: '25rem' }">
+        <div class="flex flex-column gap-3">
+          <template v-for="ingredient in ingredients" :key="ingredient.name">
+            <div class="flex flex-row gap-2">
+              <Checkbox v-model="ingredient.checked.value" :inputId="ingredient.name" :binary="true"
+                :label="ingredient.name" />
+              <label :for="ingredient.name"> {{ ingredient.name }} </label>
+            </div>
+          </template>
+        </div>
+        <div class="flex justify-content-end gap-2">
+          <Button type="button" label="Save" @click="ingredientDialogVisible = false"></Button>
+        </div>
+      </Dialog>
+
+    </div>
   </Panel>
 </template>
 
@@ -122,6 +119,7 @@ const calculatePercentage = (weight: number) => {
 .p-panel {
   padding-top: 10px;
 }
+
 .p-panel .p-panel-header {
   display: none;
 }
