b66f8965a041ffe236289d3e637552e33c3c88c4
shayd3 <ryan.ross215@gmail.com>
2024-02-26 21:57:41 -0500

    Login init

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'https://doughratio.com/',
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: 'https://doughratio.com/',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('https://doughratio.com/');
+    }
+};
+
+const firebaseConfig = {
+    // Your web app's Firebase configuration
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var app = firebase.initializeApp(firebaseConfig)
+const ui = new firebaseui.auth.AuthUI(app.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+</script>
+
+<template>
+    <div>
+        <div id="firebaseui-auth-container"></div>
+    </div>
+</template>
